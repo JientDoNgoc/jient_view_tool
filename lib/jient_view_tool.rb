33100bb7cd9ef6d1a0e96c1c208eccf9534776bc
JientDoNgoc <dongocthuyoanh.07.05@gmail.com>
2018-06-28 05:58:00 +0000

    Dev tool initial commit

---
+++ lib/jient_view_tool.rb
@@ -0,0 +1,6 @@
+require "jient_view_tool/version"
+require "jient_view_tool/renderer"
+
+module JientViewTool
+  # Your code goes here...
+end
---
+++ lib/jient_view_tool/version.rb
@@ -0,0 +1,3 @@
+module JientViewTool
+  VERSION = "0.1.0"
+end
